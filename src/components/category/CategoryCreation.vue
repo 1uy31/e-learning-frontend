d5502af8d56ecff3bad2f08f73161db00595c8bf
1uy31 <snow.tuyet.tran.31@gmail.com>
2023-07-17 00:46:15 +0300

    Install and use eslint tw plugin

--- src/components/category/CategoryCreation.vue
+++ src/components/category/CategoryCreation.vue
@@ -50,16 +50,16 @@ onMounted(() => {
 			<p v-if="formMessage.message" :class="'text-l mb-4 ' + formMessage.class">{{ formMessage.message }}</p>
 			<div class="relative mb-5 mt-2.5" data-te-input-wrapper-init>
 				<input
-					type="text"
-					class="border-1 peer block min-h-[auto] w-full rounded bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
 					id="id_new_category_name"
+					type="text"
+					class="peer block min-h-[auto] w-full rounded bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
 					placeholder="A unique name"
-					@change="(event) => setCategoryInput(event)"
 					:value="categoryName"
+					@change="(event) => setCategoryInput(event)"
 				/>
 				<label
 					for="id_new_category_name"
-					class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-cyan-700 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-cyan-800 peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+					class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-cyan-700 transition-all duration-200 ease-out peer-focus:translate-y-[-1.15rem] peer-focus:scale-[0.8] peer-focus:text-cyan-800 peer-data-[te-input-state-active]:translate-y-[-1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
 					>Category name
 				</label>
 			</div>
@@ -72,7 +72,7 @@ onMounted(() => {
 				:disabled="['', undefined].includes(categoryName)"
 				@click.prevent="submitNewCategory"
 			>
-				<SmallSpinner classes="" status="Creating" v-if="isCreatingCategory" />
+				<SmallSpinner v-if="isCreatingCategory" classes="" status="Creating" />
 				{{ !isCreatingCategory ? "Create" : "" }}
 			</button>
 		</form>
