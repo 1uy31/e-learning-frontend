e24eb0b68136a5974149826504bb6bc318a4f4a7
1uy31 <snow.tuyet.tran.31@gmail.com>
2023-07-16 10:45:12 +0300

    Use tailwind elements

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,3 @@
-import "@nordhealth/css";
-import "@nordhealth/color";
-import "@nordhealth/fonts";
-import "@nordhealth/icons";
-import "@nordhealth/tokens";
-import "@nordhealth/ag-theme-nord";
-import "@nordhealth/components";
 import { createApp, provide, h } from "vue";
 import { DefaultApolloClient } from "@vue/apollo-composable";
 import { apolloClient } from "@src/apolloClient";
