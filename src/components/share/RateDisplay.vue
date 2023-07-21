164c099294bea52628922f3026585fa9edbe8b74
1uy31 <snow.tuyet.tran.31@gmail.com>
2023-07-21 19:55:24 +0300

    Use tw in rate display component

--- src/components/share/RateDisplay.vue
+++ src/components/share/RateDisplay.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
+import FilledStarIcon from "@assets/icons/filledStar.svg?component";
+import EmptyStarIcon from "@assets/icons/emptyStar.svg?component";
+
 const props = defineProps<{ rate: number }>();
 const rating = Math.min(props.rate || 0, 5);
 </script>
 
 <template>
-	<div style="display: flex; flex-direction: row">
-		<nord-icon v-for="index in rating" :key="index" size="s" name="interface-star-filled" />
-		<nord-icon v-for="index in 5 - rating" :key="index + 5" size="s" name="interface-star" />
+	<div class="flex flex-row">
+		<FilledStarIcon v-for="index in rating" :key="index" class="text-amber-400" />
+		<EmptyStarIcon v-for="index in 5 - rating" :key="index + 5" class="text-amber-400" />
 	</div>
 </template>
 
