13eddfae54ff8c92e174ccdf5e99043bc4f90367
1uy31 <snow.tuyet.tran.31@gmail.com>
2023-07-23 13:19:50 +0300

    Add button to create new diary in parent diary view

--- src/components/diary/ParentDiaryDetail.vue
+++ src/components/diary/ParentDiaryDetail.vue
@@ -5,6 +5,7 @@ import CreatedUpdatedFooter from "@components/share/CreatedUpdatedFooter.vue";
 import RateDisplay from "@components/share/RateDisplay.vue";
 import SectionDivider from "@components/share/SectionDivider.vue";
 import EditIcon from "@assets/icons/pencilSquare.svg?component";
+import NewDiaryIcon from "@assets/icons/documentList.svg?component";
 import IconButton from "@components/share/IconButton.vue";
 
 const diaryStore = useDiaryStore();
@@ -20,7 +21,7 @@ const { selectedDiary, childDiariesByDiary } = storeToRefs(diaryStore);
 			<RateDisplay :rate="selectedDiary.rate" />
 		</div>
 
-		<div class="my-2 flex flex-row items-center">
+		<div class="my-2 flex flex-row items-center justify-between">
 			<span
 				v-if="selectedDiary.sourceUrl"
 				class="mr-2 inline-block whitespace-nowrap rounded bg-cyan-700 p-[0.55em] text-center align-baseline text-[0.75em] font-bold leading-none text-cyan-100 shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-cyan-900 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-cyan-900 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-cyan-900 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
@@ -28,11 +29,19 @@ const { selectedDiary, childDiariesByDiary } = storeToRefs(diaryStore);
 				<a :href="selectedDiary.sourceUrl" target="_blank">Source</a>
 			</span>
 
-			<IconButton>
-				<template #icon>
-					<EditIcon />
-				</template>
-			</IconButton>
+			<div class="flex flex-row items-center">
+				<IconButton class="mr-2">
+					<template #icon>
+						<NewDiaryIcon />
+					</template>
+				</IconButton>
+
+				<IconButton>
+					<template #icon>
+						<EditIcon />
+					</template>
+				</IconButton>
+			</div>
 		</div>
 
 		<SectionDivider />
@@ -42,7 +51,7 @@ const { selectedDiary, childDiariesByDiary } = storeToRefs(diaryStore);
 				v-for="childDiary in childDiariesByDiary[`${selectedDiary.id}`]"
 				:key="childDiary.id"
 				type="button"
-				class="block w-full cursor-pointer rounded-lg p-4 text-left transition duration-500 hover:bg-stone-200 hover:text-cyan-950 focus:bg-stone-300 focus:text-cyan-950 focus:ring-0"
+				class="mb-1 block w-full cursor-pointer rounded-lg bg-stone-200 p-4 text-left transition duration-500 hover:bg-stone-300 hover:text-cyan-950 focus:bg-stone-400 focus:text-cyan-950 focus:ring-0"
 				:value="childDiary.id"
 				@click="diaryStore.selectDiary(childDiary)"
 			>
