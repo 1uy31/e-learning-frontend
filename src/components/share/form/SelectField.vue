3141dfedcf03a5d811217dcf26ee16cf7020a149
1uy31 <snow.tuyet.tran.31@gmail.com>
2023-09-27 04:55:40 +0300

    Improve style of select field

--- src/components/share/form/SelectField.vue
+++ src/components/share/form/SelectField.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { initTE, Select } from "tw-elements";
-import { FORM_INPUT_LABEL_CLASS, FORM_SELECTION_CLASS, REQUIRED_MARK_CLASS } from "@src/constants/classes";
+import { FORM_SELECTION_CLASS, REQUIRED_MARK_CLASS } from "@src/constants/classes";
 import { ERROR_INFO } from "@src/constants";
 import SmallSpinner from "@components/share/SmallSpinner.vue";
 
@@ -19,26 +19,32 @@ onMounted(() => {
 </script>
 
 <template>
-	<SmallSpinner v-if="loadingOptions" class="-mt-2 mb-4 border-cyan-700" status="Loading options" />
+	<SmallSpinner v-if="loadingOptions" class="border-cyan-700" status="Loading options" />
 	<!-- UI looks buggy when v-else is used. -->
-	<div :class="'mb-6 mt-2 ' + FORM_SELECTION_CLASS" :style="[loadingOptions ? { display: 'none' } : {}]">
-		<select
-			v-bind="$attrs"
-			:id="id"
-			:required="Boolean(required)"
-			:aria-required="Boolean(required)"
-			:aria-describedby="`${id}_error`"
-			:aria-invalid="error ? true : null"
-			data-te-select-init
-		>
-			<option :v-if="!Boolean(required)"></option>
-			<slot name="options"></slot>
-		</select>
-		<label :for="id" :class="FORM_INPUT_LABEL_CLASS" data-te-select-label-ref
-			><span v-if="required" :class="REQUIRED_MARK_CLASS">*</span> {{ label }}</label
-		>
+	<div
+		:class="'mb-5 mt-2 flex flex-row items-center justify-between ' + FORM_SELECTION_CLASS"
+		:style="[loadingOptions ? { display: 'none' } : {}]"
+	>
+		<label :for="id" class="mr-2"><span v-if="required" :class="REQUIRED_MARK_CLASS">*</span> {{ label }}</label>
+		<div class="w-3/4">
+			<select
+				v-bind="$attrs"
+				:id="id"
+				:required="Boolean(required)"
+				:aria-required="Boolean(required)"
+				:aria-describedby="`${id}_error`"
+				:aria-invalid="error ? true : null"
+				data-te-select-init
+			>
+				<option v-if="!required" />
+				<slot name="options"></slot>
+			</select>
+		</div>
+		<!--		<label :for="id" :class="FORM_INPUT_LABEL_CLASS" data-te-select-label-ref-->
+		<!--			><span v-if="required" :class="REQUIRED_MARK_CLASS">*</span> {{ label }}</label-->
+		<!--		>-->
 	</div>
-	<p v-if="error" :id="`${id}_error`" :class="'text-l -mt-2 mb-5 ' + ERROR_INFO.class">
+	<p v-if="error" :id="`${id}_error`" :class="'text-l -mt-4 mb-5 ' + ERROR_INFO.class">
 		{{ error }}
 	</p>
 </template>
