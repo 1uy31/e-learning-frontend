07bb0e655d5ecb0cfd3c007a1374b810cbad9da6
1uy31 <snow.tuyet.tran.31@gmail.com>
2023-09-30 16:26:35 +0300

    Add best practice note to useReducer

--- src/composable/hooks.ts
+++ src/composable/hooks.ts
@@ -9,9 +9,17 @@ export const useState = <T>(initialState: T): [Readonly<Ref>, (newState: UnwrapR
 	return [readonly(state), setState];
 };
 
+/**
+ * * "reducer" must be pure — same inputs always result in the same output.
+ * It should not send requests, schedule timeouts, or perform any side effects
+ * (operations that impact things outside the component).
+ * It should update objects and arrays without mutations.
+ *
+ * * "action" must be used for a single user interaction, even if that leads to multiple changes in the data.
+ * */
 export const useReducer = <T, A>(
-	initialState: T,
 	reducer: (state: UnwrapRef<T>, action: A) => UnwrapRef<T>,
+	initialState: T,
 	initializer?: (state: T) => T
 ): [Readonly<Ref>, (action: A) => void] => {
 	const state = ref(initializer ? initializer(initialState) : initialState);
