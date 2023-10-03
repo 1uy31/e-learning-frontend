71d6b124bb2600f9996e14ad6ba1d1bffe85c5a4
1uy31 <snow.tuyet.tran.31@gmail.com>
2023-10-04 00:46:43 +0300

    Improve cache configuration

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -4,7 +4,25 @@ const httpLink = createHttpLink({
 	uri: "http://localhost:8000/",
 });
 
-const cache = new InMemoryCache();
+const cache = new InMemoryCache({
+	typePolicies: {
+		Query: {
+			fields: {
+				notes: {
+					keyArgs: ["diaryId"],
+					// TODO: to be improved for pagination
+					//  https://www.apollographql.com/docs/react/pagination/core-api/#defining-a-field-policy
+					merge(existing = [], incoming) {
+						return {
+							notes: [...existing, ...incoming.notes],
+							total: incoming.total,
+						};
+					},
+				},
+			},
+		},
+	},
+});
 
 export const apolloClient = new ApolloClient({
 	link: httpLink,
